9a313016590eee3533cbece4a690da3290216549
Akruti Negi <43791426+akku98@users.noreply.github.com>
2020-12-17 11:37:15 +0530

    Update transcribe.py

--- transcribe.py
+++ transcribe.py
@@ -10,7 +10,7 @@ video = me.VideoFileClip("story.mp4")
 audio = video.audio
 audio.write_audiofile("ad3.wav")
 
-apikey = 'P4BSEQ0hEaHft6frktNgenx1rz6xDwHHfDQ6tU9Oc_EG'
+apikey = '#########'
 url = 'https://api.eu-gb.speech-to-text.watson.cloud.ibm.com/instances/d91c287d-28b9-4c0d-bf75-6ce09306b525'
 
 authenticator = IAMAuthenticator(apikey)
